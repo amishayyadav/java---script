6fa2cfbec59ccc851db84f4d62acfe2812b04b7f
amishayyadav <amishayadav507@gmail.com>
2025-04-02 20:48:49 +0530

    Add files via upload

---
+++ 01_variable.js
@@ -0,0 +1,14 @@
+const accountId = 144553;
+let accountEmail = "amisha123@google.com";
+var accountPassword = "12345";
+accountCity = "Jaipur";
+
+//accountId = 2 // not allowes
+
+accountEmail = "hc@hc.com";
+accountPassword = "1732863";
+accountCity = "Bangaluru";
+
+console.log(accountId);
+
+console.table([accountId, accountEmail, accountPassword, accountCity]);
---
+++ 02_DataType.js
@@ -0,0 +1,25 @@
+"use strict"; // treat all JS code ass newer version
+
+// alert( 3 + 3) // we are using node.js not browser
+
+console.log(3 + 3);
+
+console.log("amisha");
+
+
+let name = " amisha"
+let age = 19
+let isLoggedIn = false
+
+//number => 2 to power 53
+//bignit 
+//string => ""
+// boolean => true/false
+//null =< standalone value
+// underdefined => 
+    //symbol => unique
+
+//obeject 
+
+console.log(typeof amisha);
+
